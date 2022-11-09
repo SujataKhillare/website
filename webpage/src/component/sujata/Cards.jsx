cb32a02edec02adba3c13e76926cd6cf917bd719
SujataKhillare <113751645+SujataKhillare@users.noreply.github.com>
2022-11-08 23:55:42 -0800

    update

--- webpage/src/component/sujata/Cards.jsx
+++ webpage/src/component/sujata/Cards.jsx
@@ -7,7 +7,7 @@ const Cards = () => {
     return (
         <div class="container">
             <div class ="row">
-             <div class="col-md-3">
+             <div class="col-md-3" style={{margin:'10px'}}>
              <Card style={{ width: '18rem' }}>
       <Card.Img variant="top" src="https://img.freepik.com/free-photo/top-view-pepperoni-pizza-with-mushroom-sausages-bell-pepper-olive-corn-black-wooden_141793-2158.jpg?w=2000" />
       <Card.Body>
@@ -20,7 +20,7 @@ const Cards = () => {
       </Card.Body>
     </Card>
              </div>
-             <div class="col-md-3">
+             <div class="col-md-3" style={{margin:'10px'}}>
              <Card style={{ width: '18rem' }}>
       <Card.Img variant="top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQ5GDYMXg1v7qytfrAFCJNMWP_uFwfmPz6GL2alu6g&s" />
       <Card.Body>
@@ -33,7 +33,7 @@ const Cards = () => {
       </Card.Body>
     </Card>
              </div>
-             <div class="col-md-3">
+             <div class="col-md-3" style={{margin:'10px'}}>
              <Card style={{ width: '18rem' }}>
       <Card.Img variant="top" src="https://i.ytimg.com/vi/xkMbJCtaaqA/maxresdefault.jpg" />
       <Card.Body>
@@ -46,7 +46,7 @@ const Cards = () => {
       </Card.Body>
     </Card>
              </div>
-             <div class="col-md-3">
+             <div class="col-md-3" style={{margin:'10px'}}>
              <Card style={{ width: '18rem' }}>
       <Card.Img variant="top" src="https://media.istockphoto.com/id/531487755/photo/vodka-pens.jpg?s=612x612&w=0&k=20&c=6w9P3_fMhWAl5jUDUrJcIg0GzM8RQLN6YEfXKXGjNxs=" />
       <Card.Body>
@@ -59,7 +59,7 @@ const Cards = () => {
       </Card.Body>
     </Card>
              </div>
-             <div class="col-md-3">
+             <div class="col-md-3" style={{margin:'10px'}}>
              <Card style={{ width: '18rem' }}>
       <Card.Img variant="top" src="https://i.ytimg.com/vi/xWZeQBLV6q4/maxresdefault.jpg" />
       <Card.Body>
@@ -72,7 +72,7 @@ const Cards = () => {
       </Card.Body>
     </Card>
              </div>
-             <div class="col-md-3">
+             <div class="col-md-3" style={{margin:'10px'}}>
              <Card style={{ width: '18rem' }}>
       <Card.Img variant="top" src="https://thumbs.dreamstime.com/b/indian-dosa-special-food-made-rice-lentils-south-india-33600896.jpg" />
       <Card.Body>
@@ -85,7 +85,7 @@ const Cards = () => {
       </Card.Body>
     </Card>
              </div>
-             <div class="col-md-3">
+             <div class="col-md-3" style={{margin:'10px'}}>
              <Card style={{ width: '18rem' }}>
       <Card.Img variant="top" src="https://media.istockphoto.com/id/1300342890/photo/blt-sandwich-and-fries.jpg?b=1&s=170667a&w=0&k=20&c=kE7ymKcOT7PSWuy2ZRr7xI6Iww06oiwrxQiqms7VDZo=" />
       <Card.Body>
@@ -98,7 +98,7 @@ const Cards = () => {
       </Card.Body>
     </Card>
              </div>
-             <div class="col-md-3">
+             <div class="col-md-3" style={{margin:'10px'}}>
              <Card style={{ width: '18rem' }}>
       <Card.Img variant="top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqJIazjIu6JxrOYQQsk7_PV1QyMqpejCzu_g&usqp=CAU" />
       <Card.Body>
