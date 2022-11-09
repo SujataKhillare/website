fa63d55c6708f1f35527ec0ffda1db2485002365
SujataKhillare <113751645+SujataKhillare@users.noreply.github.com>
2022-11-08 22:27:29 -0800

    SK-created footer

--- webpage/src/App.js
+++ webpage/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import ShubhangiMain from './component/shubhangi/ShubhangiMain';
 import SujataMain from './component/sujata/SujataMain';
--- webpage/src/component/sujata/SujataMain.jsx
+++ webpage/src/component/sujata/SujataMain.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Cards from './Cards';
+import Footer from './Footer';
 
 const SujataMain = () => {
   return (
-    <div>sujataMain</div>
+    <div>
+      <Cards/>
+      <Footer/>
+    </div>
   )
 }
 
